7c6466084139352f3c06766cc55cc30c790a6de1
srinu19 <srinubandlamudi55@gmail.com>
2024-01-30 12:38:50 +0530

    Added getAll Api

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -102,11 +102,35 @@ const get=async (req,res)=>{
     
 }
 
+const getAll=async (req,res)=>{
+    try {
+        const response=await cityService.getAllCities();
+        return res.status(200).json({
+            data:response,
+            success:true,
+            message:"Successfully fetched all cities",
+            err:{}
+        });
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:"Not able to get the cities",
+            err:error
+        });
+    }
+
+
+}
+
 
 module.exports={
     create,
     destroy,
     update,
-    get
+    get,
+    getAll
 
 }
